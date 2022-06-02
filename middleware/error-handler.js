a5ac0f323e678cea2f9111680b9c9d437db1fe1b
arjunsingh14 <arjunsingh140601@gmail.com>
2022-06-02 12:58:55 +0800

    added custom errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,18 @@
+import {StatusCodes} from "http-status-codes";
+
 const errorHandlerMiddleware = (error, req, res, next) => {
-    console.log(error);
-    res.status(500).json({error: 'there was an error'})
-} 
-export default errorHandlerMiddleware
+  const defaultError = {
+      statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+      msg: error.message || "Something went wrong, try again later"
+  }
+  if (error.name === 'ValidationError'){
+      defaultError.statusCode = StatusCodes.BAD_REQUEST;
+      defaultError.msg = error.message;
+  }
+  if (error.code && error.code === 11000){
+      defaultError.statusCode = StatusCodes.BAD_REQUEST;
+      defaultError.msg = `${Object.keys(error.keyValue)} has to be unique`
+  }
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
+export default errorHandlerMiddleware;
