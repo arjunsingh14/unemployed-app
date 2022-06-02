3d29311ff236ced71a6372c7bb5e202cc8c4dd59
arjunsingh14 <arjunsingh140601@gmail.com>
2022-06-02 15:05:55 +0800

    Added JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,30 @@
 import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
-const register = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: "There was an error" });
+const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!email || !name || !password) {
+    throw new BadRequestError("Please provide all values");
   }
+  const userAlreadyExists = User.findOne({ email });
+  if (!userAlreadyExists) {
+    throw new BadRequestError("The email is already registered");
+  }
+  const user = await User.create({ name, password, email });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        name: user.name,
+        lastName: user.lastName,
+        location: user.location,
+        email: user.email,
+      },
+      token,
+      location: user.location,
+    });
 };
 const login = async (req, res) => {
   res.send("login");
