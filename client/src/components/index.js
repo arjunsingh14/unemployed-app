39277f8f66143e9c379df2a49734e65324494ec1
arjunsingh14 <arjunsingh140601@gmail.com>
2022-06-05 17:04:00 +0800

    Set up navbar and logout button

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Logo from "../components/Logo"
 import Alert from "./Alert"
 import FormRow from "./FormRow"
+import Sidebar from "./Sidebar"
+import BigSidebar from "./BigSidebar"
+import Navbar from "./Navbar"
 
-export {Logo, FormRow, Alert}
+export {Logo, FormRow, Alert, Sidebar, BigSidebar, Navbar}
