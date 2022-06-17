4b57d11ea5ebbb61243373eab5903cfc8a2a225f
arjunsingh14 <arjunsingh140601@gmail.com>
2022-06-17 21:16:19 +0800

    Set up stats page structure

--- client/src/pages/Dashboard/Stats.js
+++ client/src/pages/Dashboard/Stats.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import { useAppContext } from "../../context/appContext";
+import { StatsContainer, Loading, ChartContainer } from "../../components";
 const Stats = () => {
+  const { showStats, isLoading, monthlyApplications } = useAppContext();
+  useEffect(() => {
+    showStats();
+  }, []);
+  if (isLoading) {
+    <Loading center />;
+  }
   return (
-    <div>Stats</div>
-  )
-}
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartContainer/>}
+    </>
+  );
+};
 
-export default Stats
+export default Stats;
